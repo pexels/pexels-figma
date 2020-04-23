2c050803393b73083084a76b7fbd6230aafee794
Craig Dennis <craigmdennis@gmail.com>
2020-04-24 00:06:17 +0100

    Initial curated photos load on plugin run

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,80 +1,65 @@
 import * as React from 'react';
 import useInfiniteScroll from '@closeio/use-infinite-scroll';
-// import Skeleton from 'react-loading-skeleton';
+import Skeleton from 'react-loading-skeleton';
 import Photo from './Photo';
 import SearchBar from './SearchBar';
-import randomNumber from '../utils/random-number';
-import randomNumber from '../utils/random-number';
 
 const Gallery = (props) => {
-  // Absolute values
+  // Constants
   const HEIGHT = 140;
   const WIDTH = 199;
-  const PER_PAGE = 30;
+  const PHOTOS_PER_PAGE = 30;
   const URL = 'https://api.pexels.com/v1/';
 
   // States
-  // const [pageNumber, setPageNumber] = React.useState(randomNumber);
-  // const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const [searchTerm, setSearchTerm] = React.useState('');
-
-  const [items, setItems] = React.useState([]);
+  const [photos, setPhotos] = React.useState([]);
   const [hasMore, setHasMore] = React.useState(false);
   const [pageNumber, setPageNumber] = React.useState(1);
+  const [endpoint, setEndpoint] = React.useState(`curated?`);
+
+  // Infinite Scroll
   const [page, loaderRef, scrollerRef] = useInfiniteScroll({hasMore});
 
+  // Initiate data fetching
   React.useEffect(() => {
     (async () => {
-      setPageNumber((prev) => prev + 1);
-      await fetch(`${URL}search?query=${searchTerm}&per_page=${PER_PAGE}&page=${pageNumber}`, {
-        headers: {
-          Authorization: process.env.API_KEY,
-        },
+      await fetch(`${URL}${endpoint}&per_page=${PHOTOS_PER_PAGE}&page=${pageNumber}`, {
+        headers: {Authorization: process.env.API_KEY},
       })
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
-          setHasMore(pageNumber * PER_PAGE <= data.total_results);
-          setItems((prev) => [...prev, ...data.photos]);
+          // No longer loeading
+          setLoading(false);
+
+          // Update the page number after we've loaded the previous
+          setPageNumber((prev) => prev + 1);
+
+          // Check to see if there are more pages
+          setHasMore(pageNumber * PHOTOS_PER_PAGE <= data.total_results);
+
+          // Combine the previous array of photos with the new data
+          setPhotos((prev) => [...prev, ...data.photos]);
         })
-        .catch(props.onError);
+        .catch(props.onError); // Send the error to the parent for use in the notice
     })();
   }, [page, searchTerm]);
 
   // When the seach is submitted in <SearchBar />
   const handleSearchSubmit = (term) => {
     // Clear the curated photos
-    setItems([]);
+    setPhotos([]);
+    setEndpoint(`search?query=${term}`);
+    setLoading(true);
     setSearchTerm(term);
     setPageNumber(1);
   };
 
-  // Fetch curated photos when the component is first rendered
-  // React.useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  // Fetch photos when the search state (searchTerm) is updated
-  // React.useEffect(() => {
-  //   fetchData();
-  // }, [searchTerm]);
-
-  // React.useEffect(() => {
-  //   fetchData();
-  //   console.log(pageNumber);
-  // }, [pageNumber]);
-
   // Create the gallery of Photos
-  const gallery = items.map((photo) => {
+  const gallery = photos.map((photo, i) => {
     return (
-      <Photo
-        key={photo.id}
-        photo={photo}
-        onError={props.onError}
-        onInsert={props.onInsert}
-        height={HEIGHT}
-        width={WIDTH}
-      />
+      <Photo key={i} photo={photo} onError={props.onError} onInsert={props.onInsert} height={HEIGHT} width={WIDTH} />
     );
   });
 
@@ -82,11 +67,11 @@ const Gallery = (props) => {
   return (
     <React.Fragment>
       <SearchBar onUserSubmit={handleSearchSubmit} />
-      {/* {loading && (
+      {loading && (
         <div className="skeleton">
           <Skeleton count={8} height={HEIGHT} />
         </div>
-      )} */}
+      )}
       <div id="gallery" className="gallery" ref={scrollerRef}>
         {gallery}
         {hasMore && <div ref={loaderRef}>Loading&hellip;</div>}
---
+++ src/app/components/d.ts
@@ -0,0 +1,1 @@
+declare module '@closeio/use-infinite-scroll';
