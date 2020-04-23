feaa45db269b32df14a6d2f8743f2e864afbef63
Craig Dennis <craigmdennis@gmail.com>
2020-04-23 23:21:55 +0100

    Get infinite scroll with search working

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import axios from 'axios';
-import Skeleton from 'react-loading-skeleton';
+import useInfiniteScroll from '@closeio/use-infinite-scroll';
+// import Skeleton from 'react-loading-skeleton';
 import Photo from './Photo';
 import SearchBar from './SearchBar';
+import randomNumber from '../utils/random-number';
 import randomNumber from '../utils/random-number';
 
 const Gallery = (props) => {
@@ -10,44 +11,61 @@ const Gallery = (props) => {
   const HEIGHT = 140;
   const WIDTH = 199;
   const PER_PAGE = 30;
-
-  // Enpoints
-  const searchEndpoint = 'https://api.pexels.com/v1/search';
-  const curatedEndpoint = 'https://api.pexels.com/v1/curated';
+  const URL = 'https://api.pexels.com/v1/';
 
   // States
-  const [photos, setPhotos] = React.useState([]);
-  const [pageNumber, setPageNumber] = React.useState(randomNumber);
-  const [loading, setLoading] = React.useState(false);
-  const [endpoint, setEndpoint] = React.useState(curatedEndpoint);
+  // const [pageNumber, setPageNumber] = React.useState(randomNumber);
+  // const [loading, setLoading] = React.useState(false);
   const [searchTerm, setSearchTerm] = React.useState('');
 
-  // Fetch data
-  const fetchData = async () => {
-    setLoading(true);
+  const [items, setItems] = React.useState([]);
+  const [hasMore, setHasMore] = React.useState(false);
+  const [pageNumber, setPageNumber] = React.useState(1);
+  const [page, loaderRef, scrollerRef] = useInfiniteScroll({hasMore});
 
-    return await axios
-      .get(endpoint, {
-        params: {
-          query: searchTerm,
-          per_page: PER_PAGE,
-          page: pageNumber,
-        },
+  React.useEffect(() => {
+    (async () => {
+      setPageNumber((prev) => prev + 1);
+      await fetch(`${URL}search?query=${searchTerm}&per_page=${PER_PAGE}&page=${pageNumber}`, {
         headers: {
           Authorization: process.env.API_KEY,
         },
       })
-      .then((response) => {
-        // setPhotos([...photos, ...response.data.photos]);
-        setPhotos(response.data.photos);
-        setLoading(false);
-        setPageNumber(pageNumber + 1);
-      })
-      .catch(props.onError);
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setHasMore(pageNumber * PER_PAGE <= data.total_results);
+          setItems((prev) => [...prev, ...data.photos]);
+        })
+        .catch(props.onError);
+    })();
+  }, [page, searchTerm]);
+
+  // When the seach is submitted in <SearchBar />
+  const handleSearchSubmit = (term) => {
+    // Clear the curated photos
+    setItems([]);
+    setSearchTerm(term);
+    setPageNumber(1);
   };
 
+  // Fetch curated photos when the component is first rendered
+  // React.useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // Fetch photos when the search state (searchTerm) is updated
+  // React.useEffect(() => {
+  //   fetchData();
+  // }, [searchTerm]);
+
+  // React.useEffect(() => {
+  //   fetchData();
+  //   console.log(pageNumber);
+  // }, [pageNumber]);
+
   // Create the gallery of Photos
-  const gallery = photos.map((photo) => {
+  const gallery = items.map((photo) => {
     return (
       <Photo
         key={photo.id}
@@ -60,33 +78,18 @@ const Gallery = (props) => {
     );
   });
 
-  const handleSearchSubmit = (term) => {
-    setPhotos([]);
-    setSearchTerm(term);
-    setEndpoint(searchEndpoint);
-  };
-
-  // Fetch curated photos when the component is first rendered
-  React.useEffect(() => {
-    fetchData();
-  }, []);
-
-  // Fetch photos when the search state (searchTerm) is updated
-  React.useEffect(() => {
-    fetchData();
-  }, [searchTerm]);
-
   // Show an initial group of fake images while the API request takes place
   return (
     <React.Fragment>
       <SearchBar onUserSubmit={handleSearchSubmit} />
-      {loading && (
+      {/* {loading && (
         <div className="skeleton">
           <Skeleton count={8} height={HEIGHT} />
         </div>
-      )}
-      <div id="gallery" className="gallery">
+      )} */}
+      <div id="gallery" className="gallery" ref={scrollerRef}>
         {gallery}
+        {hasMore && <div ref={loaderRef}>Loading&hellip;</div>}
       </div>
     </React.Fragment>
   );
